6d13561b6f6cefe5215bbaecdd60009a6b771f45
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-04-08 11:15:44 +0800

    Pseudocode Added

---
+++ quiz_maker.py
@@ -0,0 +1,28 @@
+#Create a program that ask user for a question.
+#It will also ask for 4 possible answer and the correct answer.
+#Write the collected data to a text file.
+#Ask another question until the user chose to exit
+
+#Function to ask number of questions to add
+
+#Function to ask for a single question and its options
+
+    #Ask user for a question
+
+    #Ask user for choices
+    
+    #Ask user for correct answer, must be in [a, b, c, d] 
+
+#Function to save quiz data to file
+
+#Main Function
+
+    #Open a file in append mode to save data
+    
+        #Loop through each question and save it
+        
+        #Ask if the user wants to continue adding questions or not
+
+    #Close File
+
+#Run program
